01317ebd1dc09c4e34f4dfc29c48b96b7f964a37
syed <syedrifai81@gmail.com>
2020-01-16 16:47:03 -0500

    added

--- src/main/java/com/example/demo/GatewayserviceApplication.java
+++ src/main/java/com/example/demo/GatewayserviceApplication.java
@@ -2,12 +2,14 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 /*
  * Zuul is built on servlet 2.5 (works with 3.x), using blocking APIs. It doesn't support any long lived connections, like websockets.
  * Gateway is built on Spring Framework 5, Project Reactor and Spring Boot 2 using non-blocking APIs. Websockets are supported and it's a much better developer experience since it's tightly integrated with Spring.
  * 
  */
 @SpringBootApplication
+@EnableCircuitBreaker
 public class GatewayserviceApplication {
 
 	public static void main(String[] args) {
