c2953e1abff1c9810ec745bd1ee7da020de4a313
syed <syedrifai81@gmail.com>
2020-02-01 15:16:06 -0600

    added

--- src/main/java/com/example/demo/GatewayserviceApplication.java
+++ src/main/java/com/example/demo/GatewayserviceApplication.java
@@ -6,7 +6,9 @@ import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 /*
  * Zuul is built on servlet 2.5 (works with 3.x), using blocking APIs. It doesn't support any long lived connections, like websockets.
  * Gateway is built on Spring Framework 5, Project Reactor and Spring Boot 2 using non-blocking APIs. Websockets are supported and it's a much better developer experience since it's tightly integrated with Spring.
+ * there hasn't integration between Swagger and Webflux yet.
  * 
+ * Gateway is built on webflux. There is no spring MVC servlet classes. There is an open issue to investigate an MVC gateway
  */
 @SpringBootApplication
 @EnableCircuitBreaker
